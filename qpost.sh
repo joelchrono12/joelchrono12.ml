4989bf6eadbeba7b51bcdbe2fcaa64ea8295ce77
joelchrono12 <joel.chrono@disroot.org>
2021-07-02 16:10:16 -0500

    removed old unused script [skip netlify]

--- qpost.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash 
-echo "Type filename"
-read name
-filename=$(date +"%Y-%m-%d"-$name)
-touch /home/joelchrono12/Documents/git/jekyll-site-test.css/_posts/$filename.md
-printf '%b\n' "$(cat ~/Documents/git/jekyll-site-test.css/_posts/_template.md)" >> /home/joelchrono12/Documents/git/jekyll-site-test.css/_posts/$filename.md
-alacritty -e nvim /home/joelchrono12/Documents/git/jekyll-site-test.css/_posts/$filename.md & disown
