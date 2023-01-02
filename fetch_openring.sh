0ec879d1de0221324d1e09b9dd2898a8b9a85cf2
joelchrono12 <joel.chrono@disroot.org>
2023-01-02 17:30:59 -0600

    updated openring

--- fetch_openring.sh
+++ fetch_openring.sh
@@ -4,4 +4,5 @@ openring \
 -s https://lazybear.io/index.xml \
 -s https://mattthequick.bearblog.dev/feed/ \
 -s https://bbbhltz.codeberg.page/rss.xml \
+-s https://moddedbear.neocities.org/feed.rss \
 < "/home/chrono/git/joelchrono12.ml/_includes/in.html"  > "/home/chrono/git/joelchrono12.ml/_includes/out.html"
