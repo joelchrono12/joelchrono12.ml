79a3bc1edb2a27608516c3e9ab431aeafdc036f7
joelchrono12 <joel.chrono@disroot.org>
2023-01-04 20:36:42 -0600

    added automatic script to toot blogs

---
+++ auto-post.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+if git rev-parse --git-dir > /dev/null 2>&1; then
+    : # This is a valid git repository
+
+    ./fetch_openring.sh
+
+    # All markdown files start with YYYY-MM-DD
+    lastpost=$(/usr/bin/ls ~/git/joelchrono12.ml/_posts/*.md | sort -r | head -n 1) 
+    getdesc=$(grep "description:" $lastpost | cut -d " " -f2-)
+    geturl=$(grep "permalink:" $lastpost | cut -d " " -f2-)
+    gettags=$(grep "tags:" $lastpost | cut -d " " -f2- | sed -r 's/([^ ]+)/#\1/g')
+    post=$(echo -e "${getdesc} \n\nhttps://joelchrono12.xyz/${geturl} \n\n${gettags} #blogpost")
+    posturl=$(toot post "$post" | cut -d/ -f5)
+    echo $posturl
+    sed -i "s/idcomments/$posturl/g" "$lastpost"
+
+    #git add *
+    #git commit -m "automatically added comments"
+    #git push
+
+    exit
+
+# directory may not be the top level.
+# Check the output of the git rev-parse command if you care
+else
+    : # this is not a git repository
+    echo "Run this inside of website folder"
+    exit
+
+fi
+
