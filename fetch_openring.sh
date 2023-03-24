db3d660a3fa5b2bedd70fd28c365aa2fffd56aba
joelchrono12 <joel.chrono@disroot.org>
2023-03-24 15:08:41 -0600

    updated openring after a while

--- fetch_openring.sh
+++ fetch_openring.sh
@@ -1,15 +1,17 @@
 openring \
--s https://www.adamsdesk.com/feed/blog.xml \
+-s https://bbbhltz.codeberg.page/rss.xml \
 -s https://kevquirk.com/feed.xml \
 -s https://lazybear.io/index.xml \
+-s https://manuelmoreale.com/feed/rss \
 -s https://mattthequick.bearblog.dev/feed/ \
--s https://bbbhltz.codeberg.page/rss.xml \
--s https://taonaw.com/feed.xml \
--s https://www.bjoreman.com/rss.rss \
+-s https://moddedbear.com/blog/index.xml \
 -s https://nayak.io/index.xml \
--s https://www.thiswildcuriosity.com/feed/ \
 -s https://rachsmith.com/rss/ \
+-s https://taonaw.com/feed.xml \
+-s https://wilw.dev/rss.xml \
 -s https://www-gem.bearblog.dev/feed/ \
--s https://moddedbear.com/blog/index.xml\
+-s https://www.adamsdesk.com/feed/blog.xml \
+-s https://www.bjoreman.com/rss.rss \
+-s https://www.thiswildcuriosity.com/feed/ \
 < "/home/chrono/git/joelchrono12.ml/_includes/in.html"  \
 > "/home/chrono/git/joelchrono12.ml/_includes/out.html"
