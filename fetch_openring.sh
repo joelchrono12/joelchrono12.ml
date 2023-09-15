b7fdf53e072810ee9e7d49b65d0420cd48f9c1ca
joelchrono12 <joel.chrono@disroot.org>
2023-09-14 20:12:39 -0600

    added some new people to the blogroll

--- fetch_openring.sh
+++ fetch_openring.sh
@@ -1,6 +1,5 @@
 openring \
 -s https://bbbhltz.codeberg.page/rss.xml \
--s https://kevquirk.com/feed.xml \
 -s https://lazybear.io/index.xml \
 -s https://manuelmoreale.com/feed/rss \
 -s https://mattthequick.bearblog.dev/feed/ \
@@ -9,10 +8,10 @@ openring \
 -s https://rachsmith.com/rss/ \
 -s https://taonaw.com/feed.xml \
 -s https://wilw.dev/rss.xml \
--s https://www-gem.bearblog.dev/feed/ \
 -s https://www.adamsdesk.com/feed/blog.xml \
 -s https://www.bjoreman.com/rss.rss \
 -s https://www.thiswildcuriosity.com/feed/ \
 -s https://noisydeadlines.net/feed/ \
+-s https://kevquirk.com/feed.xml \
 < "/home/chrono/git/joelchrono12.ml/_includes/in.html"  \
 > "/home/chrono/git/joelchrono12.ml/_includes/out.html"
