c7cc066b286c94d5cccf5bd464952f81920073b9
joelchrono12 <joel.chrono@disroot.org>
2024-02-17 21:44:26 -0600

    updated openring

--- fetch_openring.sh
+++ fetch_openring.sh
@@ -1,24 +1,23 @@
-openring \
--s https://tahimik.com/journal.rss \
--s https://0xdstn.site/index.xml \
--s https://bbbhltz.codeberg.page/rss.xml \
--s https://brandont.dev/atom.xml \
--s https://claytonerrington.com/feed.xml \
--s https://kevquirk.com/feed.xml \
--s https://lazybear.io/index.xml \
--s https://moddedbear.com/blog/index.xml \
--s https://nayak.io/index.xml \
--s https://rachsmith.com/rss/ \
--s https://warmedal.se/~bjorn/atom.xml \
--s https://manuelmoreale.com/feed/rss \
--s https://wilw.dev/rss.xml \
--s https://www.thiswildcuriosity.com/feed \
--s https://www.adamsdesk.com/feed/blog.xml \
-< "/home/chrono/git/joelchrono12.ml/_includes/in.html"  \
-> "/home/chrono/git/joelchrono12.ml/_includes/out.html"
+openring -S sources.txt -n 4 -l 128 < "/home/chrono/git/joelchrono12.ml/_includes/in.html" > "/home/chrono/git/joelchrono12.ml/_includes/out.html"
 
 #-s https://noisydeadlines.net/feed/ \
 #-s https://garrit.xyz/rss.xml \
 #-s https://mattthequick.bearblog.dev/feed/ \
 #-s https://www.bjoreman.com/rss.rss \
 #-s https://twisty.bearblog.dev/feed/ \
+# these ones work
+#-s https://tahimik.com/journal.rss \
+#-s https://0xdstn.site/index.xml \
+#-s https://bbbhltz.codeberg.page/rss.xml \
+#-s https://brandont.dev/atom.xml \
+#-s https://claytonerrington.com/feed.xml \
+#-s https://kevquirk.com/feed.xml \
+#-s https://lazybear.io/index.xml \
+#-s https://moddedbear.com/blog/index.xml \
+#-s https://nayak.io/index.xml \
+#-s https://rachsmith.com/rss/ \
+#-s https://warmedal.se/~bjorn/atom.xml \
+#-s https://manuelmoreale.com/feed/rss \
+#-s https://wilw.dev/rss.xml \
+#-s https://www.thiswildcuriosity.com/feed \
+#-s https://www.adamsdesk.com/feed/blog.xml \
