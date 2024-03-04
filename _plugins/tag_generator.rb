c8e2df3c6b6bce7cae786c7c551a0bc3cb74eadb
joelchrono12 <joel.chrono@disroot.org>
2024-03-04 09:33:11 -0600

    added css things

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -12,7 +12,7 @@ end
 def generate_tag_file(tag)
   # generate tag file
   File.open("tags/#{tag}.md", "wb") do |file|
-    file << "---\nlayout: tags\ntag-name: #{tag}\npermalink: /tags/#{tag}/\n---\n"
+    file << "---\nlayout: tags\ntag-name: #{tag}\npermalink: /more/tags/#{tag}/\n---\n"
   end
 end
 # generate feed file
